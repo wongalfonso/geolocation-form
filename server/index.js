a8edec63639c7ce461fb599cd442cc60ea8835c1
wongalofonso <alfonsowongsd@gmail.com>
2019-07-13 11:51:39 -0700

    updated npm packages

--- server/index.js
+++ server/index.js
@@ -1,16 +1,14 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.set('vew engine', 'hbs');
-app.set('views', path.join(__dirname, '../templates/views'));
-hbs.registerPartials(path.join(__dirname, '../templates/partials'));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req,res) => {
-  res.send('yes');
+  res.render('index');
 })
 
-app.listen((PORT) => console.log(`server is listening on ${PORT}`));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
